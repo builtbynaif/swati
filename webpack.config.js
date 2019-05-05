5852822cddf3f230c186c100e1d74b625568e9b9
Naif <hello@builtbynaif.com>
2019-05-05 22:16:34 +0530

    hope this works

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: {
+        App: './app/assets/scripts/App.js',
+        Vendor: './app/assets/scripts/Vendor.js'
+    },
+
+    output: {
+
+        filename: '[name].js',
+
+        path: path.resolve(__dirname, './app/temp/scripts')
+
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+      }
+    ]
+    },
+    mode: 'development'
+}
